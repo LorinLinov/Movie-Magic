06a2b2a1e7513810c05caec0c111d974f17f9515
LorinLinov <lorinlinov@abv.bg>
2026-07-06 19:37:35 +0300

    Make server and default home page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-import {express} from 'express';
-import {engine} from 'express-handlebars';
+import express from 'express';
+// import handlebars from 'express-handlebars';
 
 
+const app = express();
+
+// app.engine('handlebars', handlebars.engine());
+// app.set('view engine', 'handlebars');
+
+app.use(express.static('./src/public'));
+
+
+app.set('views', './src/views');
+
+app.get('/', (req, res) => {
+    res.sendFile('home.html', { root: './src/views' });
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000...');
+});
